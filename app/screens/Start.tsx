9c5b489c1fc8ad04ae126fa5f141d9575b9107ac
Divyanshu Negi <divyanshunegi.dn@gmail.com>
2020-07-21 23:20:42 +0530

    Initial commit

---
+++ app/screens/Start.tsx
@@ -0,0 +1,69 @@
+import {RouteProp, useRoute} from '@react-navigation/native';
+import React, {useState} from 'react';
+import {SafeAreaView, StatusBar, FlatList} from 'react-native';
+import {useValue} from 'react-native-redash';
+
+import Modal from '@components/Modal';
+import Movie from '@components/Movie';
+
+import type MovieType from '@app/types/Movie';
+import type PositionType from '@app/types/Position';
+
+interface ModalState {
+    movie: MovieType;
+    position: PositionType;
+}
+
+type StartParamList = {
+    Start: {
+        movies: Array<MovieType>;
+    };
+};
+
+type StartRoute = RouteProp<StartParamList, 'Start'>;
+
+const Start = () => {
+    const route = useRoute<StartRoute>();
+    const {movies} = route.params;
+    const activeMovieId = useValue<number>(-1);
+    const [modal, setModal] = useState<ModalState | null>(null);
+
+    const open = (index: number, movie: MovieType, position: PositionType) => {
+        activeMovieId.setValue(index);
+        setModal({movie, position});
+    };
+
+    const close = () => {
+        activeMovieId.setValue(-1);
+        setModal(null);
+    };
+
+    const renderMovies = (movie: MovieType, index: number) => {
+        return (
+            <Movie
+                activeMovieId={activeMovieId}
+                key={movie.name}
+                index={index}
+                movie={movie}
+                open={open}
+            />
+        );
+    };
+
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView>
+                <FlatList
+                    data={movies}
+                    renderItem={(movie) =>
+                        renderMovies(movie.item, movie.index)
+                    }
+                />
+                {modal !== null && <Modal {...modal} close={close} />}
+            </SafeAreaView>
+        </>
+    );
+};
+
+export default Start;
